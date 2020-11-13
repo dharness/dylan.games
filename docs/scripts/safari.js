3fe4b34e28d2aa5f554aeb64be662c085afbf532
Naila <dharness.engineer@gmail.com>
2020-11-13 13:26:11 -0500

    .

---
+++ docs/scripts/safari.js
@@ -0,0 +1,12 @@
+var isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+
+if (isSafari) {
+    var gifsForFallback = document.querySelectorAll('.gif-fallback__gif');
+    
+    gifsForFallback.forEach(g => {
+        var video = document.getElementById(g.id + '__video');
+        video.style.display = 'none';
+        g.style.display = 'unset';
+    });
+}
+
